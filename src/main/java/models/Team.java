5414ac28f6c9fb46a6bb6722ce90f1bf19fb8500
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-01 09:24:42 -0700

    Team Class constructor test, pass

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.List;
+
 public class Team {
+    private List<String> members;
+    private String teamName;
+    private String teamDescription;
+
+    public Team() {
+    }
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,11 @@ public class TeamTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void Team_instatiatesNewTeam_true() {
+        Team newTeam = new Team();
+        assertTrue(newTeam instanceof Team);
     }
 }
