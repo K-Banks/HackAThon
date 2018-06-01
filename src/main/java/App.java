1ddbb8b958a5ac90ac6b199fec14082f27c3640d
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-01 12:12:56 -0700

    fix form submission and creates new Team

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,16 +15,16 @@ public class App {
         staticFileLocation("/public");
 
         // Temporary Team Creation for testing
-//        List<String> newMembers = new ArrayList<>();
-//        newMembers.add("LawDog");
-//        Team newTeam = new Team(newMembers, "I'm the Law, Dog", "ElOhEl");
+        List<String> newMembers = new ArrayList<>();
+        newMembers.add("LawDog");
+        Team testTeam = new Team(newMembers, "I'm the Law, Dog", "ElOhEl");
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             List<Team> team = Team.getTeams();
-            if (team.size() == 0) {
+//            if (team.size() == 0) {
                 model.put("Team", team);
-            }
+//            }
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -37,16 +37,16 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             List<String> members = new ArrayList<>();
 
-            String teamName = request.params("teamName");
-            String teamDescription = request.params("teamDescription");
-            members.add(request.params("teamMembers"));
-            members.add(request.params("teamMembers2"));
-            members.add(request.params("teamMembers3"));
-            members.add(request.params("teamMembers4"));
-            members.add(request.params("teamMembers5"));
-            if (members.contains("")) {
-                members.remove("");
-            }
+            String teamName = request.queryParams("teamName");
+            String teamDescription = request.queryParams("teamDescription");
+            members.add(request.queryParams("teamMembers"));
+            members.add(request.queryParams("teamMembers2"));
+            members.add(request.queryParams("teamMembers3"));
+            members.add(request.queryParams("teamMembers4"));
+            members.add(request.queryParams("teamMembers5"));
+//            if (members.contains("")) {
+//                members.remove("");
+//            }
 
             Team newTeam = new Team(members, teamName, teamDescription);
             model.put("newTeam", newTeam);
@@ -57,11 +57,11 @@ public class App {
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
-//        get("/teams", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            List<Team> team = Team.getTeams();
-//            model.put("Team", team);
-//            return new ModelAndView(model, "index.hbs");
-//        }, new HandlebarsTemplateEngine());
+        get("/teams", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Team> team = Team.getTeams();
+            model.put("Team", team);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
