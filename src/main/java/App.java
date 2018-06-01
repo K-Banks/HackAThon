e6b485d26d7615eba50681c2d342990a93202b22
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-01 11:07:41 -0700

    Add routing for homepage. Add styling and teams list to homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,30 @@ import models.Team;
 
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
+        // Temporary Team Creation for testing
+        List<String> newMembers = new ArrayList<>();
+        newMembers.add("LawDog");
+        Team newTeam = new Team(newMembers, "I'm the Law, Dog", "ElOhEl");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Team> team = Team.getTeams();
+            if (team.size() > 0) {
+                model.put("Team", team);
+            }
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
